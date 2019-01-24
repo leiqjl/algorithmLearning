10217bba42e223d26a55e1361ac217bab6fcef08
leiqjl <leiqjl@gmail.com>
2019-01-25 00:57:33 +0800

    add bfs for paths

---
+++ src/com/algorithm/BreadthFirstPaths.java
@@ -0,0 +1,51 @@
+package com.algorithm;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public BreadthFirstPaths(Graph g, int s) {
+        marked = new boolean[g.V()];
+        edgeTo = new int[g.V()];
+        this.s = s;
+        bfs(g, s);
+    }
+
+    private void bfs(Graph g, int s) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        marked[s] = true;
+        queue.add(s);
+        while (!queue.isEmpty()) {
+            Integer v = queue.remove();
+            for (int w : g.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    queue.add(w);
+                }
+            }
+        }
+
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = v; i != s; i = edgeTo[i]) {
+            stack.push(i);
+        }
+        stack.push(s);
+        return stack;
+    }
+}
