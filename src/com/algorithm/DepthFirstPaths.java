974c8dfd3835f29f359dcafd1a9f0073b4bea2d2
leiqjl <leiqjl@gmail.com>
2019-01-25 00:49:01 +0800

    add dfs for paths

---
+++ src/com/algorithm/DepthFirstPaths.java
@@ -0,0 +1,42 @@
+package com.algorithm;
+
+import java.util.Stack;
+
+public class DepthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public DepthFirstPaths(Graph g, int s) {
+        marked = new boolean[g.V()];
+        edgeTo = new int[g.V()];
+        this.s = s;
+        dfs(g, s);
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(g, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = v; i != s; i = edgeTo[i]) {
+            stack.push(i);
+        }
+        stack.push(s);
+        return stack;
+    }
+}
