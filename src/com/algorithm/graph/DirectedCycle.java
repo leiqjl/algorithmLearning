ded2c9b2321b140553e04649360869e44e820dcc
qijl <leiqjl@163.com>
2019-01-27 21:23:58 +0800

    add directedCycle

---
+++ src/com/algorithm/graph/DirectedCycle.java
@@ -0,0 +1,50 @@
+package com.algorithm.graph;
+
+import java.util.Stack;
+
+public class DirectedCycle {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> cycle;
+    private boolean[] onStack;
+
+    public DirectedCycle(Digraph g) {
+        onStack = new boolean[g.V()];
+        edgeTo = new int[g.V()];
+        marked = new boolean[g.V()];
+        for (int v = 0; v < g.V(); v++) {
+            if (!marked[v]) {
+                dfs(g, v);
+            }
+        }
+    }
+
+    private void dfs(Digraph g, int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        for (int w : g.adj(v)) {
+            if (this.hasCycle()) {
+                return;
+            } else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(g, w);
+            } else if (onStack[w]) {
+                cycle = new Stack<>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+}
