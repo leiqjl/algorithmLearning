6dda9b5341e9e4922a03434fa776171eae902865
leiqjl <leiqjl@gmail.com>
2019-02-02 22:41:03 +0800

    add EdgeWeightedDigraph

---
+++ src/com/algorithm/graph/EdgeWeightedDigraph.java
@@ -0,0 +1,45 @@
+package com.algorithm.graph;
+
+import com.algorithm.Bag;
+
+public class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(DirectedEdge e) {
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v) {
+        return adj[v];
+    }
+
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> b = new Bag<>();
+        for (int v = 0; v < V; v++) {
+            for (DirectedEdge e : adj[v]) {
+                b.add(e);
+            }
+        }
+        return b;
+    }
+}
