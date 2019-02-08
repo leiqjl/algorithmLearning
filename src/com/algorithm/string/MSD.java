b8e61d8e42b92107bcd1f315760f8b535d36e0e1
leiqjl <leiqjl@gmail.com>
2019-02-08 19:20:28 +0800

    add MSD

---
+++ src/com/algorithm/string/MSD.java
@@ -0,0 +1,46 @@
+package com.algorithm.string;
+
+import com.algorithm.Insertion;
+
+public class MSD {
+    private static int R = 256;
+    private static final int M = 15;
+    private static String[] aux;
+
+    private static int charAt(String s, int d) {
+        if (d < s.length()) {
+            return s.charAt(d);
+        } else {
+            return -1;
+        }
+    }
+
+    public static void sort(String[] a) {
+        int n = a.length;
+        aux = new String[n];
+        sort(a, 0, n - 1, 0);
+    }
+
+    private static void sort(String[] a, int lo, int hi, int d) {
+        if (lo > hi) {
+            return;
+        }
+        if (hi <= lo + M) {
+            Insertion.sort(a, lo, hi, d);
+            return;
+        }
+        int[] count = new int[R + 2];
+        for (int i = lo; i <= hi; i++) {
+            count[charAt(a[i], d) + 2]++;
+        }
+        for (int r = 0; r < R + 1; r++) {
+            count[r + 1] += count[r];
+        }
+        for (int i = lo; i <= hi; i++) {
+            aux[count[charAt(a[i], d) + 1]++] = a[i];
+        }
+        for (int r = 0; r < R; r++) {
+            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
+        }
+    }
+}
