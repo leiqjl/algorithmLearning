5d33750c526d51699552393fd9ddb941b92f4617
leiqjl <leiqjl@gmail.com>
2019-02-18 19:18:18 +0800

    SkipList

--- src/com/algorithm/SkipList.java
+++ src/com/algorithm/SkipList.java
@@ -1,5 +1,7 @@
 package com.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class SkipList {
@@ -8,110 +10,127 @@ public class SkipList {
 
     private int levelCount = 1;
 
-    private Node head = new Node();  // 带头链表
+    private Node head = new Node();
 
-    private Random r = new Random();
+    private Random random = new Random();
 
-    public Node find(int value) {
-        Node p = head;
-        for (int i = levelCount - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                p = p.forwards[i];
-            }
+    public class Node {
+        private int data = -1;
+        private Node[] forwards = new Node[MAX_LEVEL];
+        private int maxLevel = 0;
+
+        public Node() {
         }
 
-        if (p.forwards[0] != null && p.forwards[0].data == value) {
-            return p.forwards[0];
-        } else {
-            return null;
+        public Node(int data, int maxLevel) {
+            this.data = data;
+            this.maxLevel = maxLevel;
+        }
+
+        @Override
+        public String toString() {
+            return "{ data: " + data + "; levels: " + maxLevel + " }";
         }
     }
 
-    public void insert(int value) {
+    public void insert(int val) {
         int level = randomLevel();
-        Node newNode = new Node();
-        newNode.data = value;
-        newNode.maxLevel = level;
-        Node update[] = new Node[level];
-        for (int i = 0; i < level; ++i) {
-            update[i] = head;
+        Node newNode = new Node(val, level);
+        Node[] tmp = new Node[level];
+        for (int i = 0; i < level; i++) {
+            tmp[i] = head;
         }
-
-        // record every level largest value which smaller than insert value in update[]
-        Node p = head;
-        for (int i = level - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                p = p.forwards[i];
+        Node node = head;
+        //每次结点对应位置
+        for (int i = level - 1; i >= 0; i--) {
+            while (node.forwards[i] != null && node.forwards[i].data < val) {
+                node = node.forwards[i];
             }
-            update[i] = p;// use update save node in search path
+            tmp[i] = node;
         }
 
-        // in search path node next node become new node forwords(next)
-        for (int i = 0; i < level; ++i) {
-            newNode.forwards[i] = update[i].forwards[i];
-            update[i].forwards[i] = newNode;
+        for (int i = 0; i < level; i++) {
+            newNode.forwards[i] = tmp[i].forwards[i];
+            tmp[i].forwards[i] = newNode;
         }
 
-        // update node hight
-        if (levelCount < level) levelCount = level;
+        //update level
+        if (levelCount < level) {
+            levelCount = level;
+        }
+
     }
 
-    public void delete(int value) {
-        Node[] update = new Node[levelCount];
-        Node p = head;
-        for (int i = levelCount - 1; i >= 0; --i) {
-            while (p.forwards[i] != null && p.forwards[i].data < value) {
-                p = p.forwards[i];
+    public Node get(int val) {
+        Node node = head;
+        for (int i = levelCount - 1; i >= 0; i--) {
+            while (node.forwards[i] != null && node.forwards[i].data < val) {
+                node = node.forwards[i];
             }
-            update[i] = p;
         }
+        if (node.forwards[0] != null && node.forwards[0].data == val) {
+            return node.forwards[0];
+        }
+        return null;
+    }
 
-        if (p.forwards[0] != null && p.forwards[0].data == value) {
-            for (int i = levelCount - 1; i >= 0; --i) {
-                if (update[i].forwards[i] != null && update[i].forwards[i].data == value) {
-                    update[i].forwards[i] = update[i].forwards[i].forwards[i];
+    public void delete(int val) {
+        Node[] tmp = new Node[levelCount];
+        Node node = head;
+        for (int i = levelCount - 1; i >= 0; i--) {
+            while (node.forwards[i] != null && node.forwards[i].data < val) {
+                node = node.forwards[i];
+            }
+            tmp[i] = node;
+        }
+        if (node.forwards[0] != null && node.forwards[0].data == val) {
+            for (int i = levelCount - 1; i >= 0; i--) {
+                if (tmp[i].forwards[i] != null && node.forwards[i].data == val) {
+                    tmp[i].forwards[i] = tmp[i].forwards[i].forwards[i];
                 }
             }
         }
+    }
+
+    public List<Node> get(int lo, int hi) {
+        Node node = get(lo);
+        List<Node> list = new ArrayList<>();
+        if (node != null) {
+            while (node != null && node.data <= hi) {
+                list.add(node);
+                node = node.forwards[0];
+            }
+        }
+        return list;
+    }
+
+    public void printAll() {
+        Node node = head;
+        while (node.forwards[0] != null) {
+            System.out.print(node.forwards[0] + " ");
+            node = node.forwards[0];
+        }
+        System.out.println();
     }
 
     // 随机 level 次，如果是奇数层数 +1，防止伪随机
     private int randomLevel() {
         int level = 1;
         for (int i = 1; i < MAX_LEVEL; ++i) {
-            if (r.nextInt() % 2 == 1) {
+            if (random.nextInt() % 2 == 1) {
                 level++;
             }
         }
-
         return level;
     }
 
-    public void printAll() {
-        Node p = head;
-        while (p.forwards[0] != null) {
-            System.out.print(p.forwards[0] + " ");
-            p = p.forwards[0];
+    public static void main(String[] args) {
+        SkipList skipList = new SkipList();
+        for (int i = 1; i <= 10; i++) {
+            skipList.insert(i);
         }
-        System.out.println();
+        skipList.printAll();
+        List<Node> nodes = skipList.get(3, 7);
+        System.out.println(nodes);
     }
-
-    public class Node {
-        private int data = -1;
-        private Node forwards[] = new Node[MAX_LEVEL];
-        private int maxLevel = 0;
-
-        @Override
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            builder.append("{ data: ");
-            builder.append(data);
-            builder.append("; levels: ");
-            builder.append(maxLevel);
-            builder.append(" }");
-
-            return builder.toString();
-        }
-    }
-
 }
