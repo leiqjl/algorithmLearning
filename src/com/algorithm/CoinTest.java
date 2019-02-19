a87e7b2cdf38064495cd31db991b92982c96e5ba
leiqjl <leiqjl@gmail.com>
2019-02-19 16:40:42 +0800

    DP

--- src/com/algorithm/CoinTest.java
+++ src/com/algorithm/CoinTest.java
@@ -1,8 +1,10 @@
 package com.algorithm;
 
 public class CoinTest {
+    private static int[] coin = {1, 3, 5};
+    private static int[] mem;
+
     public static int test(int n) {
-        int[] coin = {1, 3, 5};
         int[] arr = new int[n + 1];
         for (int i = 1; i < n + 1; i++) {
             arr[i] = Integer.MAX_VALUE;
@@ -17,9 +19,68 @@ public class CoinTest {
         return arr[n];
     }
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 12; i++) {
-            System.out.println(i+"  "+test(i));
+    public static int calCoinDP(int n) {
+        mem = new int[n+1];
+        return coinDP(n);
+    }
+
+    public static int coinDP(int n) {
+        if (n == 1 || n == 3 || n == 5) {
+            return 1;
         }
+        if (mem[n] > 0) {
+            return mem[n];
+        }
+        int a = 0;
+        if (n > 5) {
+            a = coinDP(n - 5);
+        }
+        int b = 0;
+        if (n > 3) {
+            b = coinDP(n - 3);
+        }
+        int c = 0;
+        if (n > 1) {
+            c = coinDP(n - 1);
+        }
+        int tmp = Math.min(Math.min(a, b), c) + 1;
+        mem[n] = tmp;
+        return tmp;
+    }
+
+    public static int minCoins(int money) {
+        if (money == 1 || money == 3 || money == 5) {
+            return 1;
+        }
+        boolean [][] state = new boolean[money][money + 1];
+        if (money >= 1) state[0][1] = true;
+        if (money >= 3) state[0][3] = true;
+        if (money >= 5) state[0][5] = true;
+        for (int i = 1; i < money; i++) {
+            for (int j = 1; j <= money; j++) {
+                if (state[i-1][j]) {
+                    if (j + 1 <= money) state[i][j + 1] = true;
+                    if (j + 3 <= money) state[i][j + 3] = true;
+                    if (j + 5 <= money) state[i][j + 5] = true;
+                    if (state[i][money]) {
+                        return i+1;
+                    }
+                }
+            }
+        }
+        return money;
+    }
+
+    public static void main(String[] args) {
+//        for (int i = 0; i < 12; i++) {
+//            System.out.println(i + "  " + test(i));
+//        }
+
+        System.out.println("2:"+minCoins(2));
+        System.out.println("4:"+minCoins(4));
+        System.out.println("6:"+minCoins(6));
+        System.out.println("11:"+minCoins(11));
+        System.out.println("49:"+minCoins(49));
+
     }
 }
---
+++ src/com/algorithm/DynamicTest.java
@@ -0,0 +1,92 @@
+package com.algorithm;
+
+public class DynamicTest {
+    private int[][] matrix;
+    private int minDist = Integer.MAX_VALUE;
+    private int[][] mem;
+
+    public DynamicTest(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+
+    private void minDistBT(int i, int j, int[][] matrix, int dist, int n) {
+        if (i == n - 1 && j == n - 1) {
+            if (dist < minDist) {
+                minDist = dist;
+            }
+            return;
+        }
+        if (i < n - 1) {
+            minDistBT(i + 1, j, matrix, dist + matrix[i + 1][j], n);
+        }
+        if (j < n - 1) {
+            minDistBT(i, j + 1, matrix, dist + matrix[i][j + 1], n);
+        }
+    }
+
+    public int minDistBT(int[][] a, int n) {
+        minDistBT(0, 0, a, a[0][0], n);
+        return minDist;
+    }
+
+
+    public int minDistDP(int[][] matrix, int n) {
+        int[][] states = new int[n][n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += matrix[i][0];
+            states[i][0] = sum;
+        }
+        sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += matrix[0][j];
+            states[0][j] = sum;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                states[i][j] = Math.min(states[i][j-1], states[i-1][j]) + matrix[i][j];
+            }
+        }
+        return states[n-1][n-1];
+    }
+
+    public int minDist(int[][] matrix, int n) {
+        mem = new int[n][n];
+        return minDist(n - 1, n - 1, matrix);
+    }
+
+    private int minDist(int i, int j, int[][] matrix) {
+        if (i == 0 && j == 0) {
+            return matrix[0][0];
+        }
+        if (mem[i][j] > 0) {
+            return mem[i][j];
+        }
+        int minLeft = Integer.MAX_VALUE;
+        if (j - 1 >= 0) {
+            minLeft = minDist(i, j - 1, matrix);
+        }
+        int minUp = Integer.MAX_VALUE;
+        if (i - 1 >= 0) {
+            minUp = minDist(i-1, j, matrix);
+        }
+        int currMinDist = matrix[i][j] + Math.min(minLeft, minUp);
+        mem[i][j] = currMinDist;
+        return currMinDist;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 9},
+                {2, 1, 3, 4},
+                {5, 2, 6, 7},
+                {6, 8, 4, 3}};
+        DynamicTest test = new DynamicTest(matrix);
+//        System.out.println(test.minDistBT(matrix, 4));
+        System.out.println(test.minDistDP(matrix, 4));
+        System.out.println(test.minDist(matrix, 4));
+//        System.out.println(test.minDist);
+
+    }
+}
