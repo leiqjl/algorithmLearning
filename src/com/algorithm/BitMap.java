7eb0cf109e72ce2706ef8a42af664abeb25bb3d5
leiqjl <leiqjl@gmail.com>
2019-02-20 19:33:19 +0800

    BitMap

---
+++ src/com/algorithm/BitMap.java
@@ -0,0 +1,30 @@
+package com.algorithm;
+
+public class BitMap {
+    private char[] bytes;
+    private int nbits;
+
+    public BitMap(int nbits) {
+        this.nbits = nbits;
+        this.bytes = new char[nbits / 16 + 1];
+    }
+
+    public void set(int k) {
+        if (k > nbits) {
+            return;
+        }
+        int byteIndex = k / 16;
+        int bitIndex = k % 16;
+        bytes[byteIndex] |= (1 << bitIndex);
+    }
+
+    public boolean get(int k) {
+        if (k > nbits) {
+            return false;
+        }
+        int byteIndex = k / 16;
+        int bitIndex = k % 16;
+        return (bytes[byteIndex] & (1 << bitIndex)) != 0;
+
+    }
+}
